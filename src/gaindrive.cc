d349fbbdeed13acc3893ab836c3cfdf975f16892
Kasper Peeters <kasper.peeters@phi-sci.com>
2026-05-09 12:40:03 +0100

    Fix build error.

--- src/gaindrive.cc
+++ src/gaindrive.cc
@@ -1636,7 +1636,7 @@ GainDrive::GainDrive(const std::string& db_path,
 		int offset = std::max(0,               std::stoi(qp("offset", "0")));
 
 		auto entries = store_.get_recent_songs(user, size, offset);
-		int max_br   = user_max_bitrate(store_, user);
+		int max_br   = request_max_bitrate(store_, req);
 
 		bool use_json = (fmt_of(req) == "json");
 		std::string body;
